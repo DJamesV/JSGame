ae1bad9686ecb2801b4ddf2f65f418d48ea55b16
DJamesV <124010148+DJamesV@users.noreply.github.com>
2025-07-31 20:46:15 -0700

    Header and footer issue solved (AI not as useful as was thought)

--- openPage.js
+++ openPage.js
@@ -1,21 +1,21 @@
 const cardsCanvas = document.getElementById("cards");
 var cardsData = []
 
-for(let i=0; i<8; i++){
-    cardsData.push({
-        imgSrc: 'https://picsum.photos/400/400',
-        title: 'Random Picture',
-        info: "This picture is taken from picsum.photos at 400x400 resolution."
-    })
-}
+// for(let i=0; i<8; i++){
+//     cardsData.push({
+//         imgSrc: 'https://picsum.photos/400/400',
+//         title: 'Random Picture',
+//         info: "This picture is taken from picsum.photos at 400x400 resolution."
+//     })
+// }
 
-for(let i=0; i<cardsData.length; i++){
-    cardsCanvas.innerHTML+=`
-        <div class="card">
-            <img src="${cardsData[i].imgSrc}" alt="Avatar" style="width:100%">
-            <div class="container">
-                <h4><b>${cardsData[i].title}</b></h4>
-            <p>${cardsData[i].info}</p>
-            </div>
-        </div>`
-}
+// for(let i=0; i<cardsData.length; i++){
+//     cardsCanvas.innerHTML+=`
+//         <div class="card">
+//             <img src="${cardsData[i].imgSrc}" alt="Avatar" style="width:100%">
+//             <div class="container">
+//                 <h4><b>${cardsData[i].title}</b></h4>
+//             <p>${cardsData[i].info}</p>
+//             </div>
+//         </div>`
+// }
